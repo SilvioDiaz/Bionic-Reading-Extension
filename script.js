d118268dc28acf978064e43630a7da3ba7190299
SilvioDiaz <silviodias8885@gmail.com>
2023-02-26 19:06:51 -0300

    test: working with a tag

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+  let wordsConverted = false;
+  
   const span = document.getElementsByTagName('span');
   const p = document.getElementsByTagName('p');
   
@@ -7,22 +9,30 @@
   
   if(p.length){
     transformByonicReading(p);
+  
   }
 
 
 function transformByonicReading(string){
   for(let i = 0; i < string.length; i++){ 
-    const words = string[i].textContent.split(" ");
+    const words = string[i].innerHTML.split(/(?<=>)([\w\s]+)(?=<\/)/g);
+
+    if(words.length > 1){
+      const testWord = words.map  (word => word.split(" "));
+      console.log(testWord);
+    }
 
     const boldWords =  words.map((word) =>{
       let lettersNumber = word.length > 1 ? Math.round(word.length / 2) : 1;
 
-      const boldLetters = word.slice(0,lettersNumber).bold();
-
-      if(boldLetters) return boldLetters.concat(word.slice(lettersNumber));
+      if(word.search("<a") === -1 && word.search("a>") === -1 ){
+        const boldLetters = word.slice(0,lettersNumber).bold();
+        return boldLetters.concat(word.slice(lettersNumber));
+      }    
       else return word;
     });
 
     string[i].innerHTML = boldWords.join(" ");
+
   }
 }
